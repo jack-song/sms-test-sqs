722dc39e3e3a0ac17d7db03eb3a1d0941002056b
Jack Song <jackthesong@gmail.com>
2017-04-11 16:13:57 +0300

    switch to celery

--- app/main.py
+++ app/main.py
@@ -3,16 +3,15 @@ from flask import Flask
 
 import boto3
 
-sqs = boto3.resource('sqs', region_name="us-east-1")
-sms_q = sqs.get_queue_by_name(QueueName='sms-test')
+import tasks
 
 # the all-important app variable for docker image
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	response = sms_q.send_message(MessageBody="Hello OH")
-	return "Oh, Hello World" + response.get('MessageId')
+	tasks.run_test("oh no id")
+	return "Oh, Hello World"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True, port=80)
---
+++ app/tasks.py
@@ -0,0 +1,12 @@
+import time
+
+from celery import Celery
+
+options= {'polling_interval': 10}
+
+# first argument current module name, broker doesn't need keys if set for boto already
+app = Celery('tasks', broker='sqs://', broker_transport_options=options)
+
+@app.task
+def run_test(message_id):
+    time.sleep(1)
