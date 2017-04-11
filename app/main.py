1eddeb2a6b086fc48d52c6e3944fb875d83e8d90
Jack Song <jackthesong@gmail.com>
2017-04-11 15:00:48 +0300

    typo

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	reponse = sms_q.send_message(MessageBody="Hello OH")
+	response = sms_q.send_message(MessageBody="Hello OH")
 	return "Oh, Hello World" + response.get('MessageId')
 
 if __name__ == "__main__":
