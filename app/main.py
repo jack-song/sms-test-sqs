1009d675bfbb23817583f8cc56e5eb844d4caf70
Jack Song <jackthesong@gmail.com>
2017-04-11 12:00:33 +0300

    add boto for queue communication

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 
-From flask import Flask
+from flask import Flask
+
+import boto.sqs
+from boto.sqs.message import Message
+
+sqs_conn = boto.sqs.connect_to_region(
+	"us-east"
+)
+sms_q = sqs_conn.create_queue('sms-test', 120)
 
 # the all-important app variable for docker image
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+	m = Message()
+	m.set_body('Oh, Hello World message.')
+	sms_q.write(m)
 	return "Oh, Hello World"
 
 if __name__ == "__main__":
